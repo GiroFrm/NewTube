1e50ee61ed1cb44b4d3e1c262afe81e313b3e41d
GiroFrm <girofram844@gmail.com>
2022-09-15 11:44:20 +1000

    final version newtube

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    method: 'GET',
+    url: BASE_URL ,
+    params: {
+     
+      maxResults: '50'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'd9df6c778dmsh3de5334b5567480p1faa24jsn891dbc6494a0',
+
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+  };
+
+  export const fetchFromAPI = async (url) =>{
+  const { data }= await axios.get(`${BASE_URL}/${url}`,options);
+
+  return data;
+  }
